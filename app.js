d8fc82359ba331a7e8670e171eb473ef0b75a91a
oskar wiiala <oskar.wiiala@metropolia.fi>
2022-05-09 14:05:20 +0300

    test connection and query to database works

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-import express from 'express';
+import express from "express";
+import { startDatabase, testQuery } from "./js/db";
 
 // does this work?
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('App running on http://localhost:3000/')
-})
+app.listen(3000, async () => {
+  console.log("App running on http://localhost:3000/");
+  console.log("attempting to start mariadb: ", await startDatabase());
+  console.log("attempting to fetch users: ", await testQuery());
+});
---
+++ js/db.js
@@ -0,0 +1,34 @@
+"use strict";
+import { createPool } from "mariadb";
+const pool = createPool({
+  host: "localhost",
+  user: "root",
+  password: "password123",
+  connectionLimit: 5,
+});
+
+const startDatabase = async () => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) return conn.end();
+  }
+};
+
+const testQuery = async () => {
+    let conn;
+  try {
+    conn = await pool.getConnection();
+    await conn.query("USE awas_project");
+    const users = await conn.query("SELECT * FROM users");
+    console.log('users: ', users);
+    return "success";
+  } catch (error) {
+    return error;
+  }
+};
+
+export { startDatabase, testQuery };
