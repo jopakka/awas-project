95d70ee96ca8db4aedfe39b299a0a7afb10077d6
jopakka <jopakkamies@gmail.com>
2022-05-09 15:04:50 +0300

    still broken

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,14 @@ const form = document.querySelector('#loginForm');
 const elements = form.elements;
 
 // Login action
-form.addEventListener('submit', evt => {
+form.addEventListener('submit', async evt => {
   evt.preventDefault();
   console.log('Login', elements);
+  try {
+    const response = await fetch('http://localhost:3000/login',
+        {method: 'POST'});
+    console.log('response', response);
+  } catch (e) {
+    console.error('login error', e.message);
+  }
 });
