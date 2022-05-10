af37b107194ae1267b67612f95f03aa35795982f
jopakka <jopakkamies@gmail.com>
2022-05-10 13:33:40 +0300

    userid in cookie

--- public/js/login.js
+++ public/js/login.js
@@ -25,6 +25,7 @@ form.addEventListener('submit', async evt => {
     } else {
       errorField.className = 'hidden';
       document.cookie = `token=${json.token}; SameSite=lax;`;
+      document.cookie = `userId=${json.user.id}; SameSite=lax;`;
       window.open('http://localhost:3000/secret', '_self');
     }
   } catch (e) {
