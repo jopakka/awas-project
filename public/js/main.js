e1e3780b9fe2009b94470987704a67fd027dd9a4
oskar wiiala <oskar.wiiala@metropolia.fi>
2022-05-10 11:15:09 +0300

    create product form working

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,115 @@
 "use strict";
 (async () => {
   const productList = document.querySelector("#productList");
+  const createProductFormList = document.querySelector(
+    "#createProductFormList"
+  );
   const buttonCreateProduct = document.querySelector("#createProduct");
 
   // Creates form for user input to create product
   const createProductForm = () => {
+    console.log("createProductForm");
+    if (document.querySelector("#createProductForm")) {
+      console.log("createProductForm exists");
+      // delete product form
+      document.querySelector("#createProductForm").remove();
+    } else {
+      console.log("createProductForm does not exist");
+      // create product form
 
+      // container for form
+      const createProductContainer = document.createElement("div");
+      createProductContainer.setAttribute("id", "createProductForm");
+
+      // actual form
+      const createProductFormContainer = document.createElement("form");
+      createProductFormContainer.setAttribute(
+        "id",
+        "createProductFormContainer"
+      );
+
+      // title label
+      const createProductFormLabelTitle = document.createElement("label");
+      createProductFormLabelTitle.innerHTML = "Product Title";
+
+      // title input
+      const createProductFormInputTitle = document.createElement("input");
+      createProductFormInputTitle.setAttribute("type", "text");
+      createProductFormInputTitle.setAttribute(
+        "name",
+        "createProductFormTitle"
+      );
+
+      // description label
+      const createProductFormLabelDescription = document.createElement("label");
+      createProductFormLabelDescription.innerHTML = "Product Description";
+
+      // description input
+      const createProductFormInputDescription = document.createElement("input");
+      createProductFormInputDescription.setAttribute("type", "text");
+      createProductFormInputDescription.setAttribute(
+        "name",
+        "createProductFormDescription"
+      );
+
+      // submit form
+      const createProductFormInputSubmit = document.createElement("input");
+      createProductFormInputSubmit.setAttribute("type", "submit");
+      createProductFormInputSubmit.setAttribute("value", "submit");
+
+      createProductFormLabelTitle.appendChild(createProductFormInputTitle);
+      createProductFormLabelDescription.appendChild(
+        createProductFormInputDescription
+      );
+
+      createProductFormContainer.appendChild(createProductFormLabelTitle);
+      createProductFormContainer.appendChild(createProductFormLabelDescription);
+      createProductFormContainer.appendChild(createProductFormInputSubmit);
+
+      createProductContainer.appendChild(createProductFormContainer);
+      createProductFormList.appendChild(createProductContainer);
+
+      console.log("created product form");
+
+      // click listener for submitting form
+      createProductFormInputSubmit.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log("submitted product");
+        const title = createProductFormInputTitle.value;
+        const description = createProductFormInputDescription.value;
+        document.querySelector("#createProductForm").remove();
+        loadContent(title, description);
+      });
+    }
   };
 
   // Clears product listing and calls createProductContent();
-  const loadContent = () => {
-    console.log("loadcontent");
-    productList.innerHTML = "";
-    createProductContent();
+  const loadContent = (title, description) => {
+    console.log("loadcontent", title, description);
+    // productList.innerHTML = "";
+    createProductContent(title, description);
   };
 
-  // Creates Product content via DOM 
-  const createProductContent = () => {
+  // Creates Product content via DOM
+  const createProductContent = (title, description) => {
     console.log("createproductcontent");
 
-    const productDiv = document.createElement("div");
-    productDiv.classList.add("productContent");
+    const productContainer = document.createElement("div");
+    productContainer.classList.add("productContent");
 
     const productTitle = document.createElement("h3");
     const productDescription = document.createElement("p");
 
-    productTitle.innerHTML = "new product title";
-    productDescription.innerHTML = "new product description";
+    productTitle.innerHTML = title;
+    productDescription.innerHTML = description;
 
-    productDiv.appendChild(productTitle);
-    productDiv.appendChild(productDescription);
+    productContainer.appendChild(productTitle);
+    productContainer.appendChild(productDescription);
 
     const productCard = document.createElement("div");
     productCard.classList.add("productCard");
 
-    productCard.appendChild(productDiv);
+    productCard.appendChild(productContainer);
 
     productList.appendChild(productCard);
   };
@@ -43,7 +118,7 @@
 
   buttonCreateProduct.addEventListener("click", (event) => {
     event.preventDefault();
-    createProductForm()
-    createProductContent();
+    createProductForm();
+    //createProductContent();
   });
 })();
